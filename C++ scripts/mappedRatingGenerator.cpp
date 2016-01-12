6c1674e161f342c8e647e1b5fdd2194c1c6c11bc
priyanshu singh <priyanshuid@gmail.com>
2016-01-12 19:17:13 +0530

    changed mapping code 7:17PM 12/01/2016

--- C++ scripts/mappedRatingGenerator.cpp
+++ C++ scripts/mappedRatingGenerator.cpp
@@ -26,9 +26,33 @@ void createMap(char *iPath, int mapAr[]){
     return;
 }
 
-void generateMappedRatings(char *iPath, char *oPath){
+void generateMappedRatings(char *iPath, char *oPath, int id1[], int id2[]){
+
+    ifstream in;
+    ofstream out;
+    in.open(iPath);
+    out.open(oPath);
 
 
+    while(in){
+        string str;
+        if(!getline(in, str))break;
+
+        istringstream ss(str);
+        string s1, s2, s3;
+
+        if(!getline(ss, s1, ','))break;
+        if(!getline(ss, s2, ','))break;
+        if(!getline(ss, s3, ','))break;
+
+        int x= atoi(s1.c_str());
+        int y= atoi(s2.c_str());
+
+        out<<id1[x]<<","<<id2[y]<<","<<s3<<endl;
+    }
+    in.close();
+    out.close();
+    return;
 
 }
 
@@ -36,13 +60,22 @@ int main(){
 
 
     string iMPath= "../data/maleMapping.csv";
-    string iFPath= "../data/femalMapping.csv";
+    string iFPath= "../data/femaleMapping.csv";
+
+    string rMData= "../data/mfTop5000Ratings.csv";
+    string rFData= "../data/fmTop5000Ratings.csv";
+
+    string oMPath= "../data/mfFinalRatings12Jan.csv";
+    string oFPath= "../data/fmFinalRatings12Jan.csv";
 
     memset(mMap, 0, sizeof(mMap));
     memset(fMap, 0, sizeof(fMap));
 
-    createMap(iMPath, &mMap[0u]);
-    createMap(iFPath, &fMap[0u]);
+    createMap(&iMPath[0u], mMap);
+    createMap(&iFPath[0u], fMap);
+
+    generateMappedRatings(&rMData[0u], &oMPath[0u], mMap, fMap);
+    generateMappedRatings(&rFData[0u], &oFPath[0u], fMap, mMap);
 
 
     return 0;
